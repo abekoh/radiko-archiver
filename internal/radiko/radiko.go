0d1e8e0b6d11a69615844df3e7e048a3c983dcd2
Kotaro Abe <abekoh.bcky@gmail.com>
2023-10-06 22:10:38 +0900

    update

--- internal/radiko/radiko.go
+++ internal/radiko/radiko.go
@@ -12,9 +12,6 @@ func RunScheduler(ctx context.Context, rulesPath, outDirPath string) {
 	toDispatcher := make(chan []Schedule)
 	toFetcher := make(chan Schedule)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	RunPlanner(ctx, toDispatcher, rulesPath)
 	RunDispatcher(ctx, toDispatcher, toFetcher)
 	RunFetchers(ctx, toFetcher, outDirPath, nil)
